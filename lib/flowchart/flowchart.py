201525842cf1fb7dc52f93a87a5633aff97ae986
Nikolai Chernikov <nikolai.chernikov.ru@gmail.com>
2015-11-24 23:09:16 +0100

    add debug example in "flowchart"

--- lib/flowchart/flowchart.py
+++ lib/flowchart/flowchart.py
@@ -2,9 +2,13 @@
 """
 This block is the reimplementation of FlowChart module from pyqtgraph,
 tuned for my custom needs
+
+Source: pyqtgraph, branch <devel>, commit <0976991efda1825d8f92b2462ded613bcadef188>
 Original author: Luke Campagnola
-Original code: pyqtgraph, branch <devel>, commit <0976991efda1825d8f92b2462ded613bcadef188>
+Original file: pyqtgraph/flowchart/Flowchart.py
 """
+
+
 from pyqtgraph.Qt import QtCore, QtGui, USE_PYSIDE, USE_PYQT5
 from pyqtgraph.flowchart.Node import *
 from pyqtgraph.pgcollections import OrderedDict
@@ -67,7 +71,7 @@ class Flowchart(Node):
         #self._chartGraphicsItem = FlowchartGraphicsItem(self)
         self._widget = None
         self._scene = None
-        self.processing = False ## flag that prevents recursive node updates
+        self.processing = False  ## flag that prevents recursive node updates
         
         self.widget()
         
@@ -941,3 +945,127 @@ class FlowchartWidget(dockarea.DockArea):
         
 class FlowchartNode(Node):
     pass
+
+
+
+if __name__ == '__main__':
+    import pyqtgraph as pg
+    import pyqtgraph.flowchart.library as fclib
+    from pyqtgraph.flowchart.library.common import CtrlNode
+    app = QtGui.QApplication([])
+
+    ## Create main window with a grid layout inside
+    win = QtGui.QMainWindow()
+    win.setWindowTitle('pyqtgraph example: FlowchartCustomNode')
+    cw = QtGui.QWidget()
+    win.setCentralWidget(cw)
+
+
+    layout = QtGui.QGridLayout()
+    cw.setLayout(layout)
+
+    ## Create an empty flowchart with a single input and output
+    fc = Flowchart(terminals={
+        'dataIn': {'io': 'in'},
+        'dataOut': {'io': 'out'}    
+    })
+    w = fc.widget()
+
+    layout.addWidget(fc.widget(), 0, 0, 2, 1)
+
+    ## Create two ImageView widgets to display the raw and processed data with contrast
+    ## and color control.
+    v1 = pg.ImageView()
+    v2 = pg.ImageView()
+    layout.addWidget(v1, 0, 1)
+    layout.addWidget(v2, 1, 1)
+
+    win.show()
+
+    ## generate random input data
+    data = np.random.normal(size=(100, 100))
+    data = 25 * pg.gaussianFilter(data, (5, 5))
+    data += np.random.normal(size=(100, 100))
+    data[40:60, 40:60] += 15.0
+    data[30:50, 30:50] += 15.0
+
+    ## Set the raw data as the input value to the flowchart
+    fc.setInput(dataIn=data)
+
+
+    ## At this point, we need some custom Node classes since those provided in the library
+    ## are not sufficient. Each node will define a set of input/output terminals, a
+    ## processing function, and optionally a control widget (to be displayed in the
+    ## flowchart control panel)
+
+    class ImageViewNode(Node):
+        """Node that displays image data in an ImageView widget"""
+        nodeName = 'ImageView'
+        
+        def __init__(self, name):
+            self.view = None
+            ## Initialize node with only a single input terminal
+            Node.__init__(self, name, terminals={'data': {'io': 'in'}})
+            
+        def setView(self, view):  ## setView must be called by the program
+            self.view = view
+            
+        def process(self, data, display=True):
+            ## if process is called with display=False, then the flowchart is being operated
+            ## in batch processing mode, so we should skip displaying to improve performance.
+            
+            if display and self.view is not None:
+                ## the 'data' argument is the value given to the 'data' terminal
+                if data is None:
+                    self.view.setImage(np.zeros((1, 1)))  # give a blank array to clear the view
+                else:
+                    self.view.setImage(data)
+
+        
+    class UnsharpMaskNode(CtrlNode):
+        """Return the input data passed through an unsharp mask."""
+        nodeName = "UnsharpMask"
+        uiTemplate = [
+            ('sigma',  'spin', {'value': 1.0, 'step': 1.0, 'range': [0.0, None]}),
+            ('strength', 'spin', {'value': 1.0, 'dec': True, 'step': 0.5, 'minStep': 0.01, 'range': [0.0, None]}),
+        ]
+        
+        def __init__(self, name):
+            ## Define the input / output terminals available on this node
+            terminals = {
+                'dataIn': dict(io='in'),    # each terminal needs at least a name and
+                'dataOut': dict(io='out'),  # to specify whether it is input or output
+            }                               # other more advanced options are available
+                                            # as well..
+            
+            CtrlNode.__init__(self, name, terminals=terminals)
+            
+        def process(self, dataIn, display=True):
+            # CtrlNode has created self.ctrls, which is a dict containing {ctrlName: widget}
+            sigma = self.ctrls['sigma'].value()
+            strength = self.ctrls['strength'].value()
+            output = dataIn - (strength * pg.gaussianFilter(dataIn, (sigma, sigma)))
+            return {'dataOut': output}
+
+
+    library = fclib.LIBRARY.copy()  # start with the default node set
+    library.addNodeType(ImageViewNode, [('Display',)])
+    library.addNodeType(UnsharpMaskNode, [('Image',),
+                                          ('Submenu_test', 'submenu2', 'submenu3')])
+    fc.setLibrary(library)
+
+    v1Node = fc.createNode('ImageView', pos=(0, -150))
+    v1Node.setView(v1)
+
+    v2Node = fc.createNode('ImageView', pos=(150, -150))
+    v2Node.setView(v2)
+
+    fNode = fc.createNode('UnsharpMask', pos=(0, 0))
+    fc.connectTerminals(fc['dataIn'], fNode['dataIn'])
+    fc.connectTerminals(fc['dataIn'], v1Node['data'])
+    fc.connectTerminals(fNode['dataOut'], v2Node['data'])
+    fc.connectTerminals(fNode['dataOut'], fc['dataOut'])
+
+
+
+    QtGui.QApplication.instance().exec_()
