0647907c41f7e95ce56af97733df6d6bd2c62e5f
Nikolai Chernikov <nikolai.chernikov.ru@gmail.com>
2016-02-15 02:10:48 +0100

    reworked nodes tidalEfficiency + timelag

--- lib/flowchart/nodes/n11_tidalefficiency/node_tidalefficiency.py
+++ lib/flowchart/nodes/n11_tidalefficiency/node_tidalefficiency.py
@@ -1,45 +1,47 @@
 #!/usr/bin python
 # -*- coding: utf-8 -*-
-
-from pyqtgraph.Qt import QtCore, QtGui
 from pyqtgraph import BusyCursor
 import numpy as np
 
-from pyqtgraph.parametertree import Parameter, ParameterTree
-from lib.flowchart.nodes.NodeWithCtrlWidget import NodeWithCtrlWidget
-from lib.functions.evaluatedictionary import evaluateDict, evaluationFunction
+from lib.flowchart.nodes.generalNode import NodeWithCtrlWidget, NodeCtrlWidget
 from lib.functions.TidalEfficiency import tidalEfficiency_method1, tidalEfficiency_method2, tidalEfficiency_method3
 from lib.functions.general import returnPandasDf, isNumpyDatetime
-
 
 
 class tidalEfficiencyNode(NodeWithCtrlWidget):
     """Calculate Tidal Efficiency comparing given river and groundwater hydrogrpahs"""
     nodeName = "tidalEfficiency"
-
+    uiTemplate = [
+            {'name': 'river', 'type': 'list', 'value': None, 'default': None, 'values': [None], 'tip': 'Column name with RIVER hydrograph data'},
+            {'name': 'gw', 'type': 'list', 'value': None, 'default': None, 'values': [None], 'tip': 'Column name with GROUNDWATER hydrograph data'},
+            {'name': 'datetime', 'type': 'list', 'value': None, 'default': None, 'values': [None], 'tip': 'Location of the datetime objects.'},
+            {'name': 'method', 'type': 'list', 'value': '1) STD', 'default': '1) STD', 'values': ['1) STD', '2) Cyclic amplitude', '3) Cyclic STD'], 'tip': 'Method to calculate Tidal Efficiency. Read docs'},
+            {'name': 'E = ', 'type': 'str', 'readonly': True, 'value': None}
+        ]
 
     def __init__(self, name, parent=None):
         terms = {'df': {'io': 'in'},
                  'matched_peaks': {'io': 'in'},
                  'E': {'io': 'out'}}
         super(tidalEfficiencyNode, self).__init__(name, parent=parent, terminals=terms, color=(250, 250, 150, 150))
-        self._ctrlWidget = tidalEfficiencyNodeCtrlWidget(parent=self)
+    
+    def _createCtrlWidget(self, **kwargs):
+        return tidalEfficiencyNodeCtrlWidget(**kwargs)
 
-        
     def process(self, df, matched_peaks):
         E = None
-        self._ctrlWidget.p.param('E = ').setValue(str(E))
+        df = returnPandasDf(df)
+        matched_peaks = returnPandasDf(matched_peaks)
+        self._ctrlWidget.param('E = ').setValue(str(E))
+        
         with BusyCursor():
-            df = returnPandasDf(df)
-            matched_peaks = returnPandasDf(matched_peaks)
-
             colname = [col for col in df.columns if not isNumpyDatetime(df[col].dtype)]
-            self._ctrlWidget.p.param('river').setLimits(colname)
-            self._ctrlWidget.p.param('gw').setLimits(colname)
-            colname = [None]+[col for col in df.columns if isNumpyDatetime(df[col].dtype)]
-            self._ctrlWidget.p.param('datetime').setLimits(colname)
+            self._ctrlWidget.param('river').setLimits(colname)
+            self._ctrlWidget.param('gw').setLimits(colname)
+            colname = [col for col in df.columns if isNumpyDatetime(df[col].dtype)]
+            self._ctrlWidget.param('datetime').setLimits(colname)
             
-            kwargs = self.ctrlWidget().evaluateState()
+            kwargs = self.ctrlWidget().prepareInputArguments()
             
             if kwargs['method'] == '1) STD':
                 E = tidalEfficiency_method1(df, kwargs['river'], kwargs['gw'])
@@ -60,65 +62,25 @@ class tidalEfficiencyNode(NodeWithCtrlWidget):
             else:
                 raise Exception('Method <%s> not yet implemented' % kwargs['method'])
         
-            self._ctrlWidget.p.param('E = ').setValue(str(E))
-            return {'E': E}
+        self._ctrlWidget.param('E = ').setValue(str(E))
+        return {'E': E}
 
 
 
-class tidalEfficiencyNodeCtrlWidget(ParameterTree):
-    
-    def __init__(self, parent=None):
-        super(tidalEfficiencyNodeCtrlWidget, self).__init__()
-        self._parent = parent
+class tidalEfficiencyNodeCtrlWidget(NodeCtrlWidget):
+    def __init__(self, **kwargs):
+        super(tidalEfficiencyNodeCtrlWidget, self).__init__(**kwargs)
 
-        params = self.params()
-        ## Create tree of Parameter objects
-        self.p = Parameter.create(name='params', type='group', children=params)
+    def initSignalConnections(self, update_parent=True):
+        print self.params()
+        new_update_parent = {
+            'action': 'disconnect',
+            'parameters': self.param('E = ')}
+        super(tidalEfficiencyNodeCtrlWidget, self).initSignalConnections(new_update_parent)
 
-        ## set parameter tree to <self> (parameterTreeWidget)
-        self.setParameters(self.p, showTop=False)
-        self.initConnections()
-        # save default state
-        self._savedState = self.saveState()
-
-    def initConnections(self):
-
-        self.p.sigValueChanged.connect(self._parent.update)
-        self.p.child('river').sigValueChanged.connect(self._parent.update)
-        self.p.child('gw').sigValueChanged.connect(self._parent.update)
-        self.p.child('datetime').sigValueChanged.connect(self._parent.update)
-        self.p.child('method').sigValueChanged.connect(self._parent.update)
-
-    def params(self):
-        params = [
-            {'name': 'river', 'type': 'list', 'value': None, 'default': None, 'values': [None], 'tip': 'Column name with RIVER hydrograph data'},
-            {'name': 'gw', 'type': 'list', 'value': None, 'default': None, 'values': [None], 'tip': 'Column name with GROUNDWATER hydrograph data'},
-            {'name': 'datetime', 'type': 'list', 'value': None, 'default': None, 'values': [None], 'tip': 'Location of the datetime objects.'},
-            {'name': 'method', 'type': 'list', 'value': '1) STD', 'default': '1) STD', 'values': ['1) STD', '2) Cyclic amplitude', '3) Cyclic STD'], 'tip': 'Method to calculate Tidal Efficiency. Read docs'},
-            #{'name': 'Calculate E', 'type': 'action'}
-            {'name': 'E = ', 'type': 'str', 'readonly': True, 'value': None}
-
-        ]
-        return params
-
-
-    def saveState(self):
-        return self.p.saveState()
-    
-    def restoreState(self, state):
-        self.p.restoreState(state)
-
-    def evaluateState(self, state=None):
-        """ function evaluates passed state , reading only necessary parameters,
-            those that can be passed to pandas.read_csv() as **kwargs
-
-            user should reimplement this function for each Node"""
-
-        if state is None:
-            state = self.saveState()
-        listWithDicts = evaluateDict(state['children'], functionToDicts=evaluationFunction, log=False, validArgumnets=['river', 'gw', 'datetime', 'method'])
+    def prepareInputArguments(self):
         kwargs = dict()
-        for d in listWithDicts:
-            # {'a': None}.items() >>> [('a', None)] => two times indexing
-            kwargs[d.items()[0][0]] = d.items()[0][1]
+        for param in self.params(ignore_groups=True):
+            kwargs[param.name()] = self.p.evaluateValue(param.value())
+        del kwargs['E = ']
         return kwargs
--- lib/flowchart/nodes/n12_timelag/node_timelag.py
+++ lib/flowchart/nodes/n12_timelag/node_timelag.py
@@ -1,11 +1,9 @@
 #!/usr/bin python
 # -*- coding: utf-8 -*-
-
-from pyqtgraph.parametertree import Parameter, ParameterTree
 from pyqtgraph import BusyCursor
 
 from lib.functions.evaluatedictionary import evaluateDict, evaluationFunction
-from lib.flowchart.nodes.NodeWithCtrlWidget import NodeWithCtrlWidget
+from lib.flowchart.nodes.generalNode import NodeWithCtrlWidget, NodeCtrlWidget
 from lib.functions.TimeLag import timelag_erskine1991_method
 from lib.functions.general import returnPandasDf, isNumpyDatetime
 
@@ -14,92 +12,7 @@ from lib.functions.general import returnPandasDf, isNumpyDatetime
 class timeLagNode(NodeWithCtrlWidget):
     """Calculate Timelag comparing given river and groundwater hydrogrpahs"""
     nodeName = "TimeLag"
-
-
-    def __init__(self, name, parent=None):
-        terms = {'df_gw': {'io': 'in'},
-                 'df_w': {'io': 'in'},
-                 'E': {'io': 'in'},
-                 'tlag': {'io': 'out'},
-                 }
-        super(timeLagNode, self).__init__(name, parent=parent, terminals=terms, color=(250, 250, 150, 150))
-        self._ctrlWidget = timeLagNodeCtrlWidget(parent=self)
-        
-    def process(self, df_gw, df_w, E):
-        self._ctrlWidget.p.param('tlag = ').setValue('?')
-        
-        df_gw = returnPandasDf(df_gw)
-        df_w = returnPandasDf(df_w)
-
-        colname = [col for col in df_gw.columns if not isNumpyDatetime(df_gw[col].dtype)]
-        self._ctrlWidget.p.param('gw').setLimits(colname)
-        colname = [None]+[col for col in df_gw.columns if isNumpyDatetime(df_gw[col].dtype)]
-        self._ctrlWidget.p.param('gw_dtime').setLimits(colname)
-        
-        colname = [col for col in df_w.columns if not isNumpyDatetime(df_w[col].dtype)]
-        self._ctrlWidget.p.param('river').setLimits(colname)
-        colname = [None]+[col for col in df_w.columns if isNumpyDatetime(df_w[col].dtype)]
-        self._ctrlWidget.p.param('river_dtime').setLimits(colname)
-
-        kwargs = self.ctrlWidget().evaluateState()
-        if E is None:
-            E = kwargs['E']
-        else:
-            self._ctrlWidget.p.param('E').setValue(E)  # maybe this will provoke process onceagain.
-            # and i would have to block the signals here...
-
-        with BusyCursor():
-            if kwargs['method'] == '1) Erskine 1991':
-                tlag = timelag_erskine1991_method(df_gw, kwargs['gw'], kwargs['gw_dtime'],
-                                    df_w, kwargs['river'], kwargs['river_dtime'],
-                                    E, tlag_tuple=(kwargs['t1'], kwargs['t2'], kwargs['t_step'])
-                                    )
-            else:
-                raise Exception('Method <%s> not yet implemented' % kwargs['method'])
-        
-            self._ctrlWidget.p.param('tlag = ').setValue(str(tlag))
-            return {'tlag': tlag}
-
-
-
-
-class timeLagNodeCtrlWidget(ParameterTree):
-    
-    def __init__(self, parent=None):
-        super(timeLagNodeCtrlWidget, self).__init__()
-        self._parent = parent
-
-        params = self.params()
-        ## Create tree of Parameter objects
-        self.p = Parameter.create(name='params', type='group', children=params)
-
-        ## set parameter tree to <self> (parameterTreeWidget)
-        self.setParameters(self.p, showTop=False)
-        self.initConnections()
-        # save default state
-        self._savedState = self.saveState()
-
-    def initConnections(self):
-        for d in self.params():
-            if d['type'] not in ['group', 'action']:
-                if 'readonly' in d and d['readonly'] is True:
-                    continue
-                self.p.child(d['name']).sigValueChanged.connect(self._parent.update)
-                
-        #self.p.child('river').sigValueChanged.connect(self._parent.update)
-        #self.p.child('river_dtime').sigValueChanged.connect(self._parent.update)
-        #self.p.child('gw').sigValueChanged.connect(self._parent.update)
-        #self.p.child('gw_dtime').sigValueChanged.connect(self._parent.update)
-        #self.p.child('E').sigValueChanged.connect(self._parent.update)
-        #self.p.child('t1').sigValueChanged.connect(self._parent.update)
-        #self.p.child('t2').sigValueChanged.connect(self._parent.update)
-        #self.p.child('t_step').sigValueChanged.connect(self._parent.update)
-        #self.p.child('method').sigValueChanged.connect(self._parent.update)
-        #self.p.child('make_copies').sigValueChanged.connect(self._parent.update)
-
-    
-    def params(self):
-        params = [
+    uiTemplate = [
             {'name': 'river', 'type': 'list', 'value': None, 'default': None, 'values': [None], 'tip': 'Column name with RIVER hydrograph data\nin `df_w` dataframe'},
             {'name': 'river_dtime', 'type': 'list', 'value': None, 'default': None, 'values': [None], 'tip': 'Location of the datetime objects\nin `df_w` dataframe'},
             {'name': 'gw', 'type': 'list', 'value': None, 'default': None, 'values': [None], 'tip': 'Column name with GROUNDWATER hydrograph data\nin `df_gw` dataframe'},
@@ -110,28 +23,69 @@ class timeLagNodeCtrlWidget(ParameterTree):
             {'name': 't2', 'type': 'int', 'value': 60, 'default': 60, 'limits': (0, int(10e3)), 'tip': 'Last value for timelag-iteration tuple. Read docs'},
             {'name': 't_step', 'type': 'int', 'value': 1, 'default': 1, 'limits': (1, int(10e3)), 'tip': 'Step value for timelag-iteration tuple. Read docs'},
             {'name': 'tlag = ', 'type': 'str', 'readonly': True, 'value': None}
-
         ]
-        return params
+
+    def __init__(self, name, parent=None):
+        terms = {'df_gw': {'io': 'in'},
+                 'df_w': {'io': 'in'},
+                 'E': {'io': 'in'},
+                 'tlag': {'io': 'out'},
+                 }
+        super(timeLagNode, self).__init__(name, parent=parent, terminals=terms, color=(250, 250, 150, 150))
+
+    def _createCtrlWidget(self, **kwargs):
+        return timeLagNodeCtrlWidget(**kwargs)
+
+    def process(self, df_gw, df_w, E):
+        self._ctrlWidget.param('tlag = ').setValue('?')
+        
+        df_gw = returnPandasDf(df_gw)
+        df_w = returnPandasDf(df_w)
+
+        colname = [col for col in df_gw.columns if not isNumpyDatetime(df_gw[col].dtype)]
+        self._ctrlWidget.param('gw').setLimits(colname)
+        colname = [col for col in df_gw.columns if isNumpyDatetime(df_gw[col].dtype)]
+        self._ctrlWidget.param('gw_dtime').setLimits(colname)
+        
+        colname = [col for col in df_w.columns if not isNumpyDatetime(df_w[col].dtype)]
+        self._ctrlWidget.param('river').setLimits(colname)
+        colname = [col for col in df_w.columns if isNumpyDatetime(df_w[col].dtype)]
+        self._ctrlWidget.param('river_dtime').setLimits(colname)
+
+        kwargs = self.ctrlWidget().prepareInputArguments()
+        if E is None:
+            E = kwargs['E']
+        else:
+            self._ctrlWidget.param('E').setValue(E)  # maybe this will provoke process onceagain.
+            # and i would have to block the signals here...
+
+        with BusyCursor():
+            if kwargs['method'] == '1) Erskine 1991':
+                tlag = timelag_erskine1991_method(df_gw, kwargs['gw'], kwargs['gw_dtime'],
+                                    df_w, kwargs['river'], kwargs['river_dtime'],
+                                    E, tlag_tuple=(kwargs['t1'], kwargs['t2'], kwargs['t_step']))
+            else:
+                raise Exception('Method <%s> not yet implemented' % kwargs['method'])
+        
+            self._ctrlWidget.param('tlag = ').setValue(str(tlag))
+        return {'tlag': tlag}
 
 
-    def saveState(self):
-        return self.p.saveState()
+
+
+class timeLagNodeCtrlWidget(NodeCtrlWidget):
     
-    def restoreState(self, state):
-        self.p.restoreState(state)
+    def __init__(self, **kwargs):
+        super(timeLagNodeCtrlWidget, self).__init__(**kwargs)
 
-    def evaluateState(self, state=None):
-        """ function evaluates passed state , reading only necessary parameters,
-            those that can be passed to pandas.read_csv() as **kwargs
-            user should reimplement this function for each Node"""
+    def initSignalConnections(self, update_parent=True):
+        new_update_parent = {
+            'action': 'disconnect',
+            'parameters': self.param('tlag = ')}
+        super(timeLagNodeCtrlWidget, self).initSignalConnections(new_update_parent)
 
-        if state is None:
-            state = self.saveState()
-        validArgs = [d['name'] for d in self.params()]
-        listWithDicts = evaluateDict(state['children'], functionToDicts=evaluationFunction, log=False, validArgumnets=validArgs)
+    def prepareInputArguments(self):
         kwargs = dict()
-        for d in listWithDicts:
-            # {'a': None}.items() >>> [('a', None)] => two times indexing
-            kwargs[d.items()[0][0]] = d.items()[0][1]
+        for param in self.params(ignore_groups=True):
+            kwargs[param.name()] = self.p.evaluateValue(param.value())
         return kwargs
