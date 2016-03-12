c1799daae8d665147b9af89ccddec6e6bed8e1df
Nikolai Chernikov <nikolai.chernikov.ru@gmail.com>
2016-03-12 02:07:05 +0100

    improved node GRADIENT

--- lib/flowchart/nodes/n_26_gradient/node_gradient.py
+++ lib/flowchart/nodes/n_26_gradient/node_gradient.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
 
-from lib.flowchart.nodes.generalNode import NodeWithCtrlWidget, NodeCtrlWidget
 import numpy as np
 import pandas as pd
+import pyqtgraph as pg
+from lib.flowchart.nodes.generalNode import NodeWithCtrlWidget, NodeCtrlWidget
 from lib.functions.general import isNumpyDatetime, isNumpyNumeric
-from lib.functions.devlin2003 import devlin2003pandas
+from lib.functions.devlin2003 import devlin2003pandas, devlin2003, angle2bearing
 
 
 
@@ -23,18 +24,19 @@ class gradientNode(NodeWithCtrlWidget):
             {'title': 'Timestep', 'name': 't', 'type': 'str', 'value': ''},
             
             {'title': 'Gradient', 'name': 'grad', 'type': 'float', 'value': None, 'readonly': True},
-            {'title': 'Direction', 'name': 'angle', 'type': 'float', 'value': None, 'readonly': True, 'suffix': ' degrees N'}
+            {'title': 'Direction', 'name': 'angle', 'type': 'float', 'value': None, 'readonly': True, 'suffix': ' degrees N'},
+            {'name': 'Calculate\nall timesteps', 'type': 'action'}
             ]
 
 
     def __init__(self, name, parent=None):
         terms = {'coord': {'io': 'in'},
                  'data': {'io': 'in'},
-                 'df': {'io': 'out'},
-                 'gradient': {'io': 'out'},
-                 'direction': {'io': 'out'}}
+                 'this': {'io': 'out'},
+                 'All': {'io': 'out'}}
         super(gradientNode, self).__init__(name, parent=parent, terminals=terms, color=(250, 250, 150, 150))
         self.data = None
+        self.All_out = None
     
     def _createCtrlWidget(self, **kwargs):
         return gradientNodeCtrlWidget(**kwargs)
@@ -47,7 +49,7 @@ class gradientNode(NodeWithCtrlWidget):
             self.data = data
         else:
             self.data = None
-            return dict(df=None, gradient=None, direction=None)
+            return dict(this=None, All=self.All_out)
         
         if coord is not None:
             colname = [col for col in coord.columns if isNumpyNumeric(coord[col].dtype)]
@@ -61,10 +63,10 @@ class gradientNode(NodeWithCtrlWidget):
             self.CW().connect_valueChanged2upd(self.CW().param('coords_grp', 'x'))
             self.CW().connect_valueChanged2upd(self.CW().param('coords_grp', 'y'))
         else:
-            return dict(df=None, gradient=None, direction=None)
+            return dict(this=None, All=self.All_out)
 
 
-        # now make sure all well specified in `coord` dataframe are found in `data`
+        # now make sure All well specified in `coord` dataframe are found in `data`
         well_names = coord.index.values
         for well_n in well_names:
                 if well_n not in data.columns:
@@ -83,27 +85,54 @@ class gradientNode(NodeWithCtrlWidget):
         df['z'] = np.zeros(len(df.index))
         for well_n in well_names:
             df.loc[well_n, 'z'] = float(row[well_n])
-
-
-
         gradient, direction = devlin2003pandas(df, kwargs['x'], kwargs['y'], 'z')
-        
+
         self.CW().param('grad').setValue(gradient)
         self.CW().param('angle').setValue(direction)
 
-        return dict(df=df, gradient=gradient, direction=direction)
+
+        # here we will generate large dataset of all timesteps
+        if self.CW().CALCULATE_ALL:
+            # now generate long dataframe
+            All = data.copy()
+            self.All_out = All  # pointer
+            All['gradient'] = np.zeros(len(All.index))
+            All['direction(degrees North)'] = np.zeros(len(All.index))
+            with pg.ProgressDialog("Calculating gradient for All timesteps {0}".format(len(All.index)), 0, len(All.index)) as dlg:
+                for row_i in All.index:
+                    row = All.loc[row_i]
+                    z = np.zeros(len(coord.index))
+                    for i, well_n in enumerate(well_names):
+                        z[i] = float(row[well_n])
+                    x = coord[kwargs['x']].values
+                    y = coord[kwargs['y']].values
+                    _, gradient, angle = devlin2003(np.matrix([x, y, z]).T)
+                    All.loc[row_i, 'gradient'] = gradient
+                    All.loc[row_i, 'direction(degrees North)'] = angle2bearing(angle, origin='N')[0]
+                    dlg += 1
+                    del z
+                    
+                    if dlg.wasCanceled():
+                        del All
+                        self.All_out = None
+                        break
+                        #return dict(df=df, All=self.All_out)
+                dlg += 1
+
+        return dict(this=df, All=self.All_out)
 
 
 class gradientNodeCtrlWidget(NodeCtrlWidget):
     def __init__(self, **kwargs):
         super(gradientNodeCtrlWidget, self).__init__(**kwargs)
-        self.UPDATE_T_DEFAULT = True
+        self.CALCULATE_ALL = False
 
         self.disconnect_valueChanged2upd(self.param('grad'))
         self.disconnect_valueChanged2upd(self.param('angle'))
         self.param('Datetime').sigValueChanged.connect(self.update_default_t)
+        self.param('Calculate\nall timesteps').sigActivated.connect(self.calculateAllrequsted)
 
-    def update_default_t(self, value):
+    def update_default_t(self, param, value):
         df = self.parent().data
         if df is not None:
             t_vals = df[value].values
@@ -113,6 +142,10 @@ class gradientNodeCtrlWidget(NodeCtrlWidget):
             self.param('t').setValue(t_min.strftime('%Y-%m-%d %H:%M:%S'))
             self.connect_valueChanged2upd(self.param('t'))
 
+    def calculateAllrequsted(self):
+        self.CALCULATE_ALL = True
+        self.parent().update()
+        self.CALCULATE_ALL = False
 
     def prepareInputArguments(self):
         kwargs = dict()
