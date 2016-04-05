f7140654c8daf0125d1e9b9d01286c9d6f88f51c
Nikolai Chernikov <nikolai.chernikov.ru@gmail.com>
2016-04-05 17:15:21 +0200

    bugfix: node: pickdates > update ok

--- lib/flowchart/nodes/n09_selectdates/node_pickequaldates.py
+++ lib/flowchart/nodes/n09_selectdates/node_pickequaldates.py
@@ -16,39 +16,38 @@ class pickEqualDatesNode(NodeWithCtrlWidget):
         ]
 
     def __init__(self, name, parent=None):
-        super(pickEqualDatesNode, self).__init__(name, parent=parent, terminals={'pattern': {'io': 'in'}, 'pickFrom': {'io': 'in'}, 'Out': {'io': 'out'}}, color=(250, 250, 150, 150))
+        super(pickEqualDatesNode, self).__init__(name, parent=parent, terminals={'pattern': {'io': 'in'}, 'pickFrom': {'io': 'in'}, 'Out': {'io': 'out'}}, color=(255, 170, 255, 150))
 
     def _createCtrlWidget(self, **kwargs):
         return pickEqualDatesNodeCtrlWidget(**kwargs)
 
     def process(self, pattern, pickFrom):
         gc.collect()
-        with BusyCursor():
-            df1 = pickFrom
-            df2 = pattern
+        df1 = pickFrom
+        df2 = pattern
 
-            colname = [col for col in df1.columns if isNumpyDatetime(df1[col].dtype)]
-            self._ctrlWidget.param('datetime <pickFrom>').setLimits(colname)
-            colname = [col for col in df2.columns if isNumpyDatetime(df2[col].dtype)]
-            self._ctrlWidget.param('datetime <pattern>').setLimits(colname)
-            
-            kwargs = self.ctrlWidget().prepareInputArguments()
-            
-            if kwargs['datetime <pattern>'] is None and kwargs['datetime <pickFrom>'] is None:
-                selector = df1.index.isin(df2.index)
-            elif kwargs['datetime <pattern>'] is not None and kwargs['datetime <pickFrom>'] is None:
-                selector = df1[kwargs['datetime <pickFrom>']].isin(df2.index)
-            elif kwargs['datetime <pickFrom>'] is None and kwargs['datetime <pickFrom>'] is not None:
-                selector = df1.index.isin(df2[kwargs['datetime <pattern>']])
-            elif kwargs['datetime <pattern>'] is not None and kwargs['datetime <pickFrom>'] is not None:
-                selector = df1[kwargs['datetime <pickFrom>']].isin(df2[kwargs['datetime <pattern>']])
-            selectedDf = df1[selector]
+        colname = [col for col in df1.columns if isNumpyDatetime(df1[col].dtype)]
+        self._ctrlWidget.param('datetime <pickFrom>').setLimits(colname)
+        colname = [col for col in df2.columns if isNumpyDatetime(df2[col].dtype)]
+        self._ctrlWidget.param('datetime <pattern>').setLimits(colname)
+        
+        kwargs = self.ctrlWidget().prepareInputArguments()
+        
+        if kwargs['datetime <pattern>'] is None and kwargs['datetime <pickFrom>'] is None:
+            selector = df1.index.isin(df2.index)
+        elif kwargs['datetime <pattern>'] is not None and kwargs['datetime <pickFrom>'] is None:
+            selector = df1[kwargs['datetime <pickFrom>']].isin(df2.index)
+        elif kwargs['datetime <pickFrom>'] is None and kwargs['datetime <pickFrom>'] is not None:
+            selector = df1.index.isin(df2[kwargs['datetime <pattern>']])
+        elif kwargs['datetime <pattern>'] is not None and kwargs['datetime <pickFrom>'] is not None:
+            selector = df1[kwargs['datetime <pickFrom>']].isin(df2[kwargs['datetime <pattern>']])
+        selectedDf = df1[selector]
         return {'Out': selectedDf}
 
 
 class pickEqualDatesNodeCtrlWidget(NodeCtrlWidget):
     def __init__(self, **kwargs):
-        super(pickEqualDatesNodeCtrlWidget, self).__init__(update_on_statechange=False, **kwargs)
+        super(pickEqualDatesNodeCtrlWidget, self).__init__(update_on_statechange=True, **kwargs)
 
     def prepareInputArguments(self):
         kwargs = dict()
